7b2c1bf7b008d79f91543fbd6b9f5e6d14726e0c
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2017-12-23 19:21:01 +0100

    Update play.js
    Added checkExtras method for find bonuses

--- play.js
+++ play.js
@@ -24,18 +24,31 @@ async function buyProducts(sleepTime = 1) {
 	}
 }
 
+async function checkExtras(sleepTime = 1) {
+	var extras = document.getElementById('shimmers');
+	if( extras.childNodes.length > 0 ) {
+		for(i=0;i<extras.childNodes.length;i++) {
+			var element = extras.childNodes[i];
+			console.log('Found shimmer :-)');
+			element.click();
+			await sleep(sleepTime);
+		}
+	}
+}
+
 async function play(loopTimes = 10000, sleepTime = 1) {
 	
 	while(true) {
 	
 		for(i=0;i<loopTimes;i++) {
-			console.log('click');
+			//console.log('click');
 			cookie.click();
 			await sleep(sleepTime);
+			checkExtras();
 		}
-
+		
 		buyUpgrade();
 		buyProducts();
 	}
 }
-play();
+play()
