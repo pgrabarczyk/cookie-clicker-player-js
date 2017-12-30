7ee43ffb3d9ccf671583ce8d0eca0331e6810774
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2017-12-30 02:19:26 +0100

    Update play.js
    Added getMostProfitableProduct (in progress)

--- play.js
+++ play.js
@@ -1,83 +1,25 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function getMostProfitableProduct() {
+	//TODO iterate of all unlocked and calculate var cashPerCookie = getPriceForProduct / cookiesPerProduct;
 }
 
-var cookie = document.getElementById("bigCookie");
+function getPriceForProduct(productId) {
+	return Game.ObjectsById[productId].price;
+}
 
-async function buyUpgrades(sleepTime = 1) {
-	var upgrades = document.getElementById("upgrades");
-	for( i=0; i< upgrades.childNodes.length ; i++) {
-		var upgrade = upgrades.childNodes[i];
-		if( upgrade.classList.contains("enabled") ) {
-			upgrade.click();
-			await sleep(sleepTime);
-			buyUpgrades();
-			break;
+function cookiesPerProduct(productId) {
+	var tooltip = Game.ObjectsById[productId].tooltip();
+	tooltip = tooltip.replace(/&bull;/g,'');
+
+	var parser = new DOMParser();
+	var tooltipDOM = parser.parseFromString(tooltip , "text/xml");
+
+	var children = tooltipDOM.childNodes[0].childNodes;
+	for( i=0; i< children.length; i++) {
+		if( children[i].tagName === 'div' && children[i].classList.contains("data") ) {
+			console.log( children[i] );
+			//TODO cut price from text then return as var
 		}
 	}
 }
 
-function getMostExpensiveUnclockedProductId() {
-	var products = document.getElementsByClassName("product unlocked");
-	var lastProductId = products.length-1;	
-	return lastProductId;
-}
-
-function getMostExpensiveProductId() {
-	var products = document.getElementsByClassName("product unlocked enabled");
-	var lastProductId = products.length-1;
-	return lastProductId; 
-}
-
-function isMakeSenseToUpgradeProduct() {
-	var productId = getMostExpensiveProductId();
-	return isProductOwnedLessThan(productId);
-}
-
-function isProductOwnedLessThan(productId, threshold = 31) {
-	const maxProductId = 14;
-	if( productId === maxProductId ) {
-		return true;
-	}
-	var productOwned = document.getElementById("productOwned" + productId);
-	return (productOwned.innerHTML < threshold);
-}
-
-async function buyProducts(sleepTime = 1) {
-	while( getMostExpensiveProductId() != -1 && isMakeSenseToUpgradeProduct() ) {
-		var productId = "product" + getMostExpensiveProductId();
-		var product = document.getElementById(productId);
-		product.click();
-		await sleep(sleepTime);
-	}
-}
-
-async function checkExtras(sleepTime = 1) {
-	var extras = document.getElementById('shimmers');
-	if( extras.childNodes.length > 0 ) {
-		for(i=0;i<extras.childNodes.length;i++) {
-			var element = extras.childNodes[i];
-			console.log('Found shimmer :-)');
-			element.click();
-			await sleep(sleepTime);
-		}
-	}
-}
-
-async function play(loopTimes = 10000, sleepTime = 1) {
-	
-	while(true) {
-	
-		for(i=0;i<loopTimes;i++) {
-			//console.log('click');
-			cookie.click();
-			await sleep(sleepTime);
-			checkExtras();
-		}
-		
-		buyUpgrades();
-		buyProducts();
-	}
-}
-
-play()
+cookiesPerProduct(0);
